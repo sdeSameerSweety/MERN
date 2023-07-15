c39f4cbe27838a7a39105f9ced65ad04150a15ae
SameerSweety Swain <137391842+sdeSameerSweety@users.noreply.github.com>
2023-07-15 16:52:39 +0000

    added Immediately Invoked Function Expression (IIFE) in index.js file

--- MERN+MARN/HTML/index.js
+++ MERN+MARN/HTML/index.js
@@ -14,3 +14,28 @@ function f(a, b) {
     return sum;
   }
   console.log(f(3, 4)); // 7
+
+  // Immediately Invoked Function Expression (IIFE)
+  //Step-1
+  var f = function(a, b) {
+    const sum = a + b;
+    return console.log(sum);
+  }
+  f(3, 4);
+
+  //Step-2
+  var f = function(a, b) {
+    const sum = a + b;
+    return console.log(sum);
+  }
+  
+ (function(a, b) {
+    const sum = a + b;
+    return console.log(sum)})(3, 4); // or write this f() both are same
+ //Step-3
+
+  const result = (function(a, b) {
+    const sum = a + b;
+    return console.log(sum);
+  })(3, 4); // its save the loc 
+  
