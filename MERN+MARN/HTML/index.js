3d07549e7690dc38d2d93f4544cebd548086709b
SameerSweety Swain <137391842+sdeSameerSweety@users.noreply.github.com>
2023-07-15 16:32:52 +0000

    added Anonymous Function in index.js file

--- MERN+MARN/HTML/index.js
+++ MERN+MARN/HTML/index.js
@@ -5,3 +5,12 @@ function f(a, b) {
     return sum; // You Can't Export/Return Value After it 
   }
   console.log(f(3, 4)); // 7
+
+
+  // Anonymous Function
+
+  var f = function(a, b) {
+    const sum = a + b;
+    return sum;
+  }
+  console.log(f(3, 4)); // 7
