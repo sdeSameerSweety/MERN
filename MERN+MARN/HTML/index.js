902e15191c9721a2ec41bde6b2e01e605168fe12
SameerSweety Swain <137391842+sdeSameerSweety@users.noreply.github.com>
2023-07-17 13:23:59 +0000

    index.js file has been updated

--- MERN+MARN/HTML/index.js
+++ MERN+MARN/HTML/index.js
@@ -1,91 +1,91 @@
 // Standard Function Syntax
 
 function f(a, b) {
+  const sum = a + b;
+  return sum; // You Can't Export/Return Value After it 
+}
+console.log(f(3, 4)); // 7
+
+
+// Anonymous Function
+
+var f = function(a, b) {
+  const sum = a + b;
+  return sum;
+}
+console.log(f(3, 4)); // 7
+
+// Immediately Invoked Function Expression (IIFE)
+//Step-1
+var f = function(a, b) {
+  const sum = a + b;
+  return console.log(sum);
+}
+f(3, 4);
+
+//Step-2
+var f = function(a, b) {
+  const sum = a + b;
+  return console.log(sum);
+}
+
+(function(a, b) {
+  const sum = a + b;
+  return console.log(sum)})(3, 4); // or write this f() both are same
+//Step-3
+
+const result = (function(a, b) {
+  const sum = a + b;
+  return console.log(sum);
+})(3, 4); // its save the loc 
+
+// Functions Within Functions
+
+function createFunction() {
+  function f(a, b) {
     const sum = a + b;
-    return sum; // You Can't Export/Return Value After it 
+    return sum;
   }
-  console.log(f(3, 4)); // 7
+  return f;
+}
+const f = createFunction();
+console.log(f(3, 4)); // 7
 
+// Function Hoisting
 
-  // Anonymous Function
-
-  var f = function(a, b) {
+function createFunction() {
+  return f;
+  function f(a, b) {
     const sum = a + b;
     return sum;
   }
-  console.log(f(3, 4)); // 7
+}
+const f = createFunction();
+console.log(f(3, 4)); // 7
 
-  // Immediately Invoked Function Expression (IIFE)
-  //Step-1
-  var f = function(a, b) {
-    const sum = a + b;
-    return console.log(sum);
-  }
-  f(3, 4);
+//Closures
 
-  //Step-2
-  var f = function(a, b) {
-    const sum = a + b;
-    return console.log(sum);
-  }
-  
- (function(a, b) {
-    const sum = a + b;
-    return console.log(sum)})(3, 4); // or write this f() both are same
- //Step-3
-
-  const result = (function(a, b) {
-    const sum = a + b;
-    return console.log(sum);
-  })(3, 4); // its save the loc 
-  
-  // Functions Within Functions
-
-  function createFunction() {
-    function f(a, b) {
-      const sum = a + b;
-      return sum;
-    }
-    return f;
-  }
-  const f = createFunction();
-  console.log(f(3, 4)); // 7
-
- // Function Hoisting
-
- function createFunction() {
-    return f;
-    function f(a, b) {
-      const sum = a + b;
-      return sum;
-    }
-  }
-  const f = createFunction();
-  console.log(f(3, 4)); // 7
-
-  //Closures
-
-  function createAdder(a) {
-    function f(b) {
-      const sum = a + b;
-      return sum;
-    }
-    return f;
-  }
-  const f = createAdder(3);
-  console.log(f(4)); // 7
-
-  //Arrow Syntax
-
-  const f = (a, b) => {
+function createAdder(a) {
+  function f(b) {
     const sum = a + b;
     return sum;
-  };
-  console.log(f(3, 4)); // 7
+  }
+  return f;
+}
+const f = createAdder(3);
+console.log(f(4)); // 7
 
-  // Omit Return
+//Arrow Syntax
 
-  const f = (a, b) => a + b;
+const f = (a, b) => {
+  const sum = a + b;
+  return sum;
+};
+console.log(f(3, 4)); // 7
+
+// Omit Return
+
+const f = (a, b) => a + b;
 console.log(f(3, 4)); // 7
 
 //Value Vs Refferance 
@@ -104,10 +104,10 @@ let array = [1, 2 , 3 ];
 let array2 = array;
 
 //copy via refferance
-let array = [1, 2 , 3 ];
-let array2 = array;
-console.log(array);
-console.log(array2);
+let array3 = [1, 2 , 3 ];
+let array4 = array;
+console.log(array3);
+console.log(array4);
 array2.pop() 
-console.log(array)
-console.log(array2)
+console.log(array3)
+console.log(array4)
